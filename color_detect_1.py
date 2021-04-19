17e49e78030b48e9c5d99c8f1aaa42865c79c43f
ONC-HNIW <onc.gi@nnkk.work>
2021-04-19 22:31:29 +0900

    update color_detect_1.py

--- color_detect_1.py
+++ color_detect_1.py
@@ -1,23 +1,23 @@
 import numpy as np
 import cv2
 
-filepath = "1.mp4"
+filepath = ""
 
 video = cv2.VideoCapture(filepath)
 
 while(video.isOpened()):
     ret, frame = video.read()
-    frame_hsv = cv2.cvtColor(frame, hsv, COLOR_BGR2HSV)
+    frame_hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
 	
     hsv_low = np.array([0,0,0])
     hsv_upper = np.array([100,100,100])
     frame_mask = cv2.inRange(frame_hsv, hsv_low, hsv_upper)
 	
-    frame_out = cv2.bitwise_and(frame, frame, mask=frame_mask)
+    frame_out = cv2.bitwise_and(frame_hsv, frame_hsv, mask=frame_mask)
     frame_out_2 = frame_out
-	
+
 	# 面積・重心計算付きのラベリング処理を行う
-    num_labels, label_image, stats, center = cv2.connectedComponentsWithStats(bin_image)
+    num_labels, label_image, stats, center = cv2.connectedComponentsWithStats(frame_mask)
 
     # 最大のラベルは画面全体を覆う黒なので不要．データを削除
     num_labels = num_labels - 1
@@ -26,7 +26,7 @@ while(video.isOpened()):
 
     # 検出したラベルの数だけ繰り返す
     for index in range(num_labels):
-        # ラベルのx,y,w,h,面積s,重心位置mx,myを取り出す
+        #ラベルのx,y,w,h,面積s,重心位置mx,myを取り出す
         x = stats[index][0]
         y = stats[index][1]
         w = stats[index][2]
@@ -34,7 +34,7 @@ while(video.isOpened()):
         s = stats[index][4]
         mx = int(center[index][0])
         my = int(center[index][1])
-        #print("(x,y)=%d,%d (w,h)=%d,%d s=%d (mx,my)=%d,%d"%(x, y, w, h, s, mx, my) )
+        print("(x,y)=%d,%d (w,h)=%d,%d s=%d (mx,my)=%d,%d"%(x, y, w, h, s, mx, my) )
 
         # ラベルを囲うバウンディングボックスを描画
         cv2.rectangle(frame_out_2, (x, y), (x+w, y+h), (255, 0, 255))
@@ -44,7 +44,7 @@ while(video.isOpened()):
         cv2.putText(frame_out_2, "%d"%(s), (x, y+h+30), cv2.FONT_HERSHEY_PLAIN, 1, (255, 255, 0))
 
         # (X)ウィンドウに表示
-        cv2.imshow('OpenCV Window', frame_out_2)  # ウィンドウに表示するイメージを変えれば色々表示できる
+        cv2.imshow('OpenCV Window', frame_out_2)
         
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
